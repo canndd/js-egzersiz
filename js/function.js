a1e17972b161bfb1c8a5bb27973c459f28dc4052
Can demir <cannddemirr@gmail.com>
2022-10-18 22:58:59 +0300

    Arttır azalt 1.yöntem

--- js/function.js
+++ js/function.js
@@ -1,4 +1,21 @@
 let counter = 0;
-let counterDOM=document.querySelector("#caunter");
+let counterDOM = document.querySelector("#caunter");
 let increaseDOM = document.querySelector("#increase");
 let decreaseDOM = document.querySelector("#decrease");
+
+counterDOM.innerHTML = counter;
+
+increaseDOM.addEventListener("click",DOMclick);
+decreaseDOM.addEventListener("click",DOMclick);
+
+function DOMclick(){
+
+    console.log(this.id)
+    if(this.id == "increase"){
+        counterDOM.innerHTML = counter +=1
+    } else 
+    counterDOM.innerHTML= counter -= 1
+        
+
+
+}
